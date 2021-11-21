475dc6684119d0a638bed589407d3f59268fff5a
Kiran <kiran.venugopal@actiotech.com>
2021-11-21 17:40:44 +0530

    minor correction

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'react-flex-grid'
+import 'react-flex-grid/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
